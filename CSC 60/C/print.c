1094c0912a82ab322309b687d6429d6d08b4a87d
Junior <49306278+jvniorrr@users.noreply.github.com>
2022-09-22 10:03:42 -0700

    Create print.c

---
+++ CSC 60/C/print.c
@@ -0,0 +1,16 @@
+// HEADER FILES
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    #define COUNT 10
+
+    printf("Hello world\n");
+
+    for (int i=0; i<COUNT; i++) {
+        printf("i = %d\n", i);
+    }
+
+    return 0;
+    
+}
