323879788cec94de07033a891401416c17d8564f
Junior <49306278+jvniorrr@users.noreply.github.com>
2022-10-10 10:39:08 -0700

    CSC 60 quizzes
    - Add week 4 quiz
    - Remove Control Structure quiz

---
+++ CSC 60/C/Week 4/quiz.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /* Q1
+    int x;
+    char ch;
+    int y;
+    scanf("%d%c%d", &x, &ch, &y);
+    printf("The value of ch is %d\n", ch);
+    // 32
+    */
+
+    /* Q2
+    int x = 20;
+    int y = 30;
+    scanf("%3d%2d", &x, &y);
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+
+    // 203, 45
+     */
+
+    /* Q3
+    int x = 20;
+    int y = 30;
+    scanf("%*d %2d", &y);
+    printf("The value of x is %d\n", x);
+    printf("The value of y is %d\n", y);
+    x=20, y=34
+    */
+
+    /* Q4
+     int x = 20;
+     int y;
+
+     #include <stdio.h>
+
+     scanf("%d", y);
+    //  include statement should be the first line
+     */
+
+    /* Q5
+     int x;
+     char ch;
+     int y;
+
+     scanf("%d%c%d", &x, &ch, &y);
+     printf("The value of ch is %d\n", ch);
+    //  10
+     */
+
+    /* Q6
+    int x;
+    int y;
+    int numRead = scanf("%d%d", &x, &y);
+
+    printf("The number of items read is %d\n", numRead);
+    // 1
+    */
+
+    /* Q7
+      int x;
+      char ch;
+      int y;
+      int numRead = scanf("%d%c%d", &x, &ch, &y);
+      printf("The number of items read is %d\n", numRead);
+    //   3
+    */
+
+    /* Q8
+    char ch1, ch2, ch3;
+    int numRead = scanf("%c%c%c", &ch1, &ch2, &ch3);
+    printf("The number of items read is %d\n", numRead);
+    // 3
+    */
+
+    /* Q9
+    char ch1, ch2, ch3;
+    ch1 = getchar();
+
+    scanf("%c", &ch2);
+    printf("%c %c \n", ch1, ch2);
+    // true
+    */
+
+    /* Q10
+    char ch1, ch2, ch3;
+    int ch;
+
+    ch1 = getchar();
+    ch3 = getchar();
+    ch2 = getchar();
+
+    printf("%c\n", ch3);
+    printf("%c %c \n", ch1, ch2);
+    // true
+    */
+
+    /* Q11
+    float value;
+    value = 789.34; //
+    printf("%f\n", value);
+    value = 78934e-2;
+    printf("%f\n", value);
+    // True
+    */
+
+    /**
+     * Q12 = True
+     */
+
+    /* Q13
+    char val = 'E';
+    printf("%d\n", val); // True
+    */
+
+    /* Q14 = True
+    int x;
+    scanf("%d", x);
+    printf("%d", x);
+    */
+
+    /* Q15
+     int x = 5;
+     scanf("%*d%d", &x);
+     printf("%d\n", x); 
+     // 30
+     */
+
+    /* Q16
+    int i = 5;
+    scanf("%*c %d", &i);
+    printf("%d\n", i); 
+    // 0
+    */
+
+    /* Q19
+    int x;
+    char ch;
+    int y;
+    scanf("d", &x);
+    */
+
+    /* Q20
+    int x;
+    char ch;
+    int y;
+    scanf(%d, x);
+    */
+
+    /* Q22
+    int x;
+    char ch;
+    int y;
+    scanf("%d", x);
+    // 65
+    */
+
+   /* Q23 = 97 */
+
+   /* Q24 = A */
+   /*
+   char ch = getchar();
+   ch = getchar();
+   printf("%c\n", ch);
+   */
+   /** Q25 = B */
+
+    return 0;
+}
